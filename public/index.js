e1d30685fbd33f48d9af8b018d5af304b74ac201
dougsillars <doug.sillars@gmail.com>
2021-09-22 19:52:17 -0400

    ready to try webhook

---
+++ public/index.js
@@ -0,0 +1,157 @@
+
+window.onload  = function(){ 
+
+    const watermarkInput = document.getElementById('watermark-file');
+    const watermarkName = document.getElementById('watermark-filename');
+
+    
+    //update div with filename on change
+    watermarkInput.addEventListener('change',() =>{
+     ///   console.log(watermarkInput.files[0]);
+        watermarkName.innerHTML = watermarkInput.files[0].name;
+
+    });
+
+    const backgroundInput = document.getElementById('background-file');
+    const backgroundName = document.getElementById('background-filename');
+   
+        //update div with filename on change
+        backgroundInput.addEventListener('change',() =>{
+     //       console.log(backgroundInput.files[0]);
+            backgroundName.innerHTML = backgroundInput.files[0].name;
+    
+        });
+
+    const input = document.querySelector('#video-file');
+        
+
+    var url ="https://sandbox.api.video/upload?token=";
+    var chunkCounter =0;
+    //break into 1 MB chunks for demo purposes
+    //new mim chunk size is 5MB (august 2021)
+    const chunkSize = 6000000;  
+    var videoId = "";
+    var playerUrl = "";
+    var file;
+    var filename;
+    var numberofChunks;
+
+    input.addEventListener('change', () => {
+        console.log("click!");
+        //upload the images and create tokens and upload files.  But first.. did they give us the 2 images we need?
+        if(backgroundInput.files[0] && watermarkInput.files[0]){
+            //we can upload stuff
+              //first we're goig to call getDelegatedTokeb to create 2 videoIDs, get a token, and uplaod the 2 images
+            getDelegatedToken(videoName.value, videoDescription.value, backgroundInput.files[0], watermarkInput.files[0]);
+            function getDelegatedToken(title, description, bgImage, wmImage){
+                videoId="";
+                console.log(title, description);
+                var qReq = new XMLHttpRequest();
+                var bumperForm = new FormData();
+
+                //add the 2 files.
+               bumperForm.append('backgroundFile', bgImage,backgroundInput.files[0].name );
+                bumperForm.append('watermarkFile', wmImage, watermarkInput.files[0].name);
+                bumperForm.append('title', title);
+                 bumperForm.append('description', description);
+                console.log("bumperForm",bumperForm);
+                qReq.open("POST", "/createVideo");
+                
+                qReq.onload = function (oEvent) {
+                    
+                    console.log(qReq.status);
+                    //once we create the video id do stuff
+                    if(qReq.status == 429){
+                        //rate limited
+                        document.getElementById("token-information").innerHTML = "Your uploads have been rate limited. We allow 5 uploads/hour with this demo app. Please try again in a bit."
+                    }
+                    else{
+                        console.log("token in response");
+                        // images are uploaded to the server and available for viewing.
+                        //  delegated token is returned, 
+                        // videoId is retruned.
+                        
+                        var tokenVideoId = JSON.parse(qReq.response);
+                        
+                        console.log(tokenVideoId);
+                        //TODO grabVideoId and token ID
+                        videoId = tokenVideoId.videoId;
+                        bgPath = tokenVideoId.bgFile;
+                        wmPath = tokenVideoId.wmFile;
+                        var delegatedToken = tokenVideoId.token;
+                        const uploader = new VideoUploader({
+                            file: input.files[0],
+                            videoId: videoId,
+                            //changed to sandbox, becuase we cannot have nice things
+                            uploadToken: delegatedToken,
+                            chunkSize: 1024*1024*10, // 10MB
+                            retries: 10
+                        });
+                        uploader.onProgress((event) => {
+                            var percentComplete = Math.round(event.currentChunkUploadedBytes / event.chunksBytes * 100);
+                            var totalPercentComplete = Math.round(event.uploadedBytes / event.totalBytes * 100);
+                            document.getElementById("chunk-information").innerHTML = "Chunk # " + event.currentChunk + " is " + percentComplete + "% uploaded. Total uploaded: " + totalPercentComplete +"%";
+                        })
+                        uploader.upload()
+                            .then((video) => {
+                                console.log(video);
+                                playerUrl = video.assets.player;
+                                console.log("Images & video uploaded! We are now creating your video with bumpers." ) ;
+                                document.getElementById("video-information").innerHTML = "Images & video uploaded! We are now creating your video with bumpers." ;
+                                uploadTheBumperText();
+                            });
+                    }
+                }
+                qReq.send(bumperForm);
+
+            
+                function uploadTheBumperText() {
+                    const introTitle = document.getElementById('introTitle');
+                    const introTitle2 = document.getElementById('introTitle2');
+                    const outroTitle = document.getElementById('outroTitle');
+                    const outroTitle2 = document.getElementById('outroTitle2');
+                    var pReq = new XMLHttpRequest();
+                    var titleJson = {
+                        "introTitle":introTitle,
+                        "introTitle2":introTitle2,
+                        "outroTitle":outroTitle,
+                        "outroTitle2":outroTitle2,
+                        "videoId": videoId,
+                        "bgpath": bgPath,
+                        "wmpath": wmPath
+                    };
+                    console.log ("titleJson", titleJson);
+
+                    pReq.open("POST", '/trackprogress' );
+                    pReq.setRequestHeader("Content-type", "application/json");
+                    pReq.onload = function (oEvent) {
+                        //this means that the mp4 was created - all the stuff sent to Shotstack, 
+                        //the video created and uplaoded to api.video, and the 720p verison is readyto watch
+                        var finalurl = pReq.response;
+                        document.getElementById("video-information").innerHTML = "video created at api.video "+ finalurl;
+                    }
+                    pReq.send(JSON.stringify(titleJson));
+
+
+                }
+            
+            
+            
+            
+            
+                
+            
+                    
+                    
+                
+            }
+
+   
+
+        }else{
+            document.getElementById("token-information").innerHTML = "Please add both a watermark image and a background image";
+
+        }
+
+    });
+}
--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ const express = require('express');
 //express for the website and pug to create the pages
 const app = express();
 bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '2Gb'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','pug');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-var request = require("request");
+
+//files
+var fs = require('fs');
+const formidable = require('formidable');
+const path = require('path');
+
 
 //rate limiting to protect the demos
 const rateLimit = require("express-rate-limit");
@@ -24,126 +30,396 @@ app.use(
   );
 
 
+
+//shotstack SDK
+const Shotstack = require('shotstack-sdk');
+const defaultClient = Shotstack.ApiClient.instance;
+const DeveloperKey = defaultClient.authentications['DeveloperKey'];
+const api = new Shotstack.EditApi();
+//set to shotstack staging (for now)
+let apiUrl = 'https://api.shotstack.io/stage';
+defaultClient.basePath = apiUrl;
+DeveloperKey.apiKey = process.env.SSapiKey;
+
+
+
+
 //apivideo
 //const apiVideo = require('@api.video/nodejs-sdk');
 //august 2021 update to the new Node JS api client
 const apiVideoClient = require('@api.video/nodejs-client');
+const { default: CaptionsListResponse } = require('@api.video/nodejs-client/lib/model/CaptionsListResponse');
+const { Edit } = require('shotstack-sdk');
+
+
 
 //if you chnage the key to sandbox or prod - make sure you fix the delegated toekn on the upload page
 const apiVideoKey = process.env.apiProductionKey;
+const SSapikey = process.env.SSapiKey;
 client = new apiVideoClient({ apiKey: apiVideoKey});
 
 
-// website demo
+
+
+
 //get request is the initial request - load the HTML page with the form
 app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../public', 'index.html'));  
+		//res.sendFile(path.join(__dirname, '../public', 'index.html'));  
 });
 
 
 app.post('/createVideo', (req,res) => {
-	console.log("request body",req.body);
+	
+	console.log("createvideo");
 	//now we'll make 2 rewquests to apivideo
 	//1 create a videoId for the uppload, with any desired params
 	//2 create a delegated token for the upload
-	var public = true;
-	var title = req.body.title;
-	var descr = req.body.description;
-
-	console.log("title", title);
 	
-	
-	let result = client.videos.create({	"title": title, "mp4Support": mp4,
-		"public": public, 
-		"description": descr,					
-	});
-	console.log(result);
-	result.then(function(video) {
-		console.log(video);
-		var videoId = video.videoId;
-		console.log(videoId);
-		//create a delegated token. The Node API client supports this.
-		//const tokenCreationPayload = ''; // 
-		//tokenCreationPayload.setTtl()=90; // Time in seconds that the token will be active. A value of 0 means that the token has no exipration date. The default is to have no expiration.
-		const ttl = 90;
-		let tokenResult = client.uploadTokens.createToken({"ttl": ttl});
-		tokenResult.then(function (token){
-			console.log("token",token);
-			var delegatedToken = token.token;
-			var tokenExpiry = token.expiresAt;
-			console.log("new token", delegatedToken);
-			console.log("new token expires", tokenExpiry);
-			var tokenVideoIdJson = {"token": delegatedToken,
-									"expires":tokenExpiry,
-									"videoId": videoId};
-			res.setHeader('Content-Type', 'application/json');
-			res.end(JSON.stringify(tokenVideoIdJson));
+	var form = new formidable.IncomingForm({maxFileSize : 2000 * 1024 * 1024}); //2 Gb
+	console.log(form);
+	form.parse(req, (err, fields, files) => {
 
-		});
-
-		/*
-		//ok have a new videoId for the video - now create a delegated token
-		//since the new delegated token with TTL is not yet in the Node SDK, I'll have to authenticate 
-		//and then request a token - 2 calls to api.video
-		var authOptions = {
-			method: 'POST',
-			url: 'https://ws.api.video/auth/api-key',
-			headers: {
-				accept: 'application/json'
-				
-			},
-			json: {"apiKey":apiVideoKey}
-
+		if (err) {
+			console.error('form Error', err);
+			throw err;
 		}
-		console.log(authOptions);	
-		request(authOptions, function (error, response, body) {
-			if (error) throw new Error(error);
-			//this will give me the api key
-			
-			var authToken = body.access_token;
-			console.log(authToken);
-			//now use this to generate a delegated toke with a ttl of 90s
-			var tokenTTL = 90;
-			var tokenOptions = {
-				method: 'POST',
-				url: 'https://ws.api.video/upload-tokens',
-				headers: {
-					accept: 'application/json',
-					authorization: 'Bearer ' +authToken
-				},
-				json: {"ttl":tokenTTL}
-	
-			}
-			request(tokenOptions, function (error, response, body) {
-				if (error) throw new Error(error);
-				var delegatedToken = body.token;
-				var tokenExpiry = body.expiresAt;
+
+
+		var title = fields.title;
+		var descr = fields.description;
+		console.log(title, descr);
+		console.log("BG data", JSON.stringify(files.backgroundFile));
+		console.log("WM data", JSON.stringify(files.watermarkFile));
+		//now lets move these 2 images to /public/images - so they are on the server
+		var newPath = __dirname + "/../public/images/";
+		console.log("newPath",newPath);
+		var bgPath = files.backgroundFile.path;
+		var bgName = path.parse(files.backgroundFile.name).name;
+		
+		var newbgPath = newPath + bgName ;
+		var wmPath = files.watermarkFile.path;
+		var wmName = path.parse(files.watermarkFile.name).name;
+
+		var newWmPath = newPath +wmName;
+		fs.rename(bgPath, newbgPath, function (err) {
+			if (err) throw err;
+		 console.log('File uploaded and moved!');
+		//FILE IS RENAMED
+			fs.rename(wmPath, newWmPath, function (err) {
+				if (err) throw err;
+			console.log('File uploaded and moved!');
+			//FILE IS RENAMED
+			});
+	  	});
+
+        
+		let result = client.videos.create({	"title": title +" initial video",
+			"description": descr					
+		});
+		console.log(result);
+		result.then(function(video) {
+			console.log(video);
+			var videoId = video.videoId;
+			console.log(videoId);
+			//create a delegated token. The Node API client supports this.
+			//const tokenCreationPayload = ''; // 
+			//tokenCreationPayload.setTtl()=90; // Time in seconds that the token will be active. A value of 0 means that the token has no exipration date. The default is to have no expiration.
+			const ttl = 90;
+			let tokenResult = client.uploadTokens.createToken({"ttl": ttl});
+			tokenResult.then(function (token){
+				console.log("token",token);
+				var delegatedToken = token.token;
+				var tokenExpiry = token.expiresAt;
 				console.log("new token", delegatedToken);
 				console.log("new token expires", tokenExpiry);
 				var tokenVideoIdJson = {"token": delegatedToken,
 										"expires":tokenExpiry,
-										"videoId": videoId};
+										"videoId": videoId,
+										//also need to send the background image and watermakr filenames - to resend back to the server
+										"bgFile" : newbgPath,
+										"wmFile" :newWmPath
+										};
 				res.setHeader('Content-Type', 'application/json');
 				res.end(JSON.stringify(tokenVideoIdJson));
 
 			});
 
-
 			
-	 	   
+			
+			
+			
+			
+		}).catch((error) => {
+			console.log(error);
 		});	
-		*/
 		
-	}).catch((error) => {
-		console.log(error);
-	});	
-
+	});
 
 
 
 });
 
 
+app.post('/trackprogress', (req,res) => {
+	var reqBody = (req.body);
+	// we need all the title text strings
+	const introTitle = req.introTitle;
+	const introTitle2 = req.introTitle2;
+	const outroTitle = req.outroTitle;
+	const outroTitle2 =req.outroTitle2;
+
+	const bgpath = req.bgpath;
+	const wmpath = req.wmpath;
+
+	//we need the videoId and the mp4 encoding type to check the webhook responses
+	var videoId = reqBody.videoId;
+	var encodingType = "mp4";
+	//has this videoId had a MP4 yet?
+	function checkWebhook(videoId, encodingType, webhooks){
+		foundMatch = false;
+		console.log("there are " + webhooks.length + " webhook entries to scan");
+		for(var i=0;i<webhooks.length;i++){
+			if(webhooks[i].videoId === videoId && webhooks[i].encoding === encodingType){
+				//we have a match!!
+				foundMatch = true;
+				//need mp4 URL now that it is created
+				 getmp4 = client.videos.get(videoId);
+				 getmp4.then(function (mp4Result){ 
+					console.log("mp4Result", mp4Result);
+					//grab mp4 URL
+					const mp4Url = mp4Result.assets.mp4;
+					getStatus = client.videos.getStatus(videoId);
+					getStatus.then(function (statusResult){
+						console.log("statusResult",statusResult);
+						//grab the duration	
+						const duration = statusResult.encoding.duration;
+						console.log("mp4URL & duration", mp4Url + " " + duration);
+
+
+						//create our query for Shotstack
+						//shotstack has a Node SDK
+						//send to Shotstack
+
+						//create the assets.. each asset goes in a clip
+
+						//video asset
+						let videoAsset = new Shotstack.VideoAsset;
+							videoAsset.setSrc(mp4Url);
+							videoAsset.volume(0.5);
+						let videoClip =  new Shotstack.Clip;
+							videoClip.setAsset(videoAsset);
+							videoClip.setStart(4);
+							videoClip.setLength(duration);
+
+						//image assets
+						let backgroundAsset = new Shotstack.ImageAsset;
+							backgroundAsset.setSrc('https://bumper.a.video/images/'+bgpath)
+						//2 clips for tha background - one at front one at back
+						let introBGClip = new Shotstack.Clip;
+							introBGClip.setAsset(backgroundAsset);
+							introBGClip.setStart(0);
+							introBGClip.setLength(4);
+						let outroBGClip = new Shotstack.Clip;
+							outroBGClip.setAsset(backgroundAsset);
+							outroBGClip.setStart(4+duratiion);
+							outroBGClip.setLength(4);
+
+
+						let watermarkAsset = new Shotstack.ImageAsset;
+							watermarkAsset.setSrc('https://bumper.a.video/images/'+wmpath)
+						//watermark to run full video 
+						let watermarkClip = new Shotstack.Clip;
+							watermarkClip.setAsset(watermarkAsset);
+							watermarkClip.setStart(0);
+							watermarkClip.setLength(4+duration);
+
+						//text assets
+						let titleIntroTextAsset =new Shotstack.TitleAsset;
+							titleIntroTextAsset.setText(introTitle);
+							titleIntroTextAsset.setStyle('chunk');
+							titleIntroTextAsset.setColor('#ffffff');
+							titleIntroTextAsset.setSize('x-large');
+							titleIntroTextAsset.position('center');
+						let titleIntroClip = new Shotstack.Clip;
+							titleIntroClip.setAsset(titleIntroTextAsset);
+							titleIntroClip.setStart(0.1);
+							titleIntroClip.setLength(3.9);
+
+						let titleIntroTextAsset2 =new Shotstack.TitleAsset;
+							titleIntroTextAsset2.setText(introTitle2);
+							titleIntroTextAsset2.setStyle('chunk');
+							titleIntroTextAsset2.setColor('#ffffff');
+							titleIntroTextAsset2.setSize('large');
+							titleIntroTextAsset2.position('bottom');
+						let titleIntroClip2 = new Shotstack.Clip;
+							titleIntroClip2.setAsset(titleIntroTextAsset2);
+							titleIntroClip2.setStart(0.2);
+							titleIntroClip2.setLength(3.8);
+
+						let titleouttroTextAsset =new Shotstack.TitleAsset;
+							titleouttroTextAsset.setText(outroTitle);
+							titleouttroTextAsset.setStyle('chunk');
+							titleouttroTextAsset.setColor('#ffffff');
+							titleouttroTextAsset.setSize('x-large');
+							titleouttroTextAsset.position('center');
+						let titleOutroClip = new Shotstack.Clip;
+							titleOutroClip.setAsset(titleOutroTextAsset);
+							titleOutroClip.setStart(0.1);
+							titleOutroClip.setLength(3.9);
+
+
+						let titleouttroTextAsset2 =new Shotstack.TitleAsset;
+							titleouttroTextAsset2.setText(outroTitle2);
+							titleouttroTextAsset2.setStyle('chunk');
+							titleouttroTextAsset2.setColor('#ffffff');
+							titleouttroTextAsset2.setSize('large');
+							titleouttroTextAsset2.position('bottom');
+						let titleOutroClip2 = new Shotstack.Clip;
+							titleOutroClip2.setAsset(titleOutroTextAsset2);
+							titleOutroClip2.setStart(0.2);
+							titleOutroClip2.setLength(3.8);
+
+						//all the assets in clips go in tracks
+
+						let watermarkTrack = new Shotstack.Track;
+							watermarkTrack.setClips([watermarkClip]);
+						let mainTrack = new Shotstack.Track;
+							mainTrack.setClios([introBGClip, titleIntroClip, titleIntroClip2, videoClip, outroBGClip, titleOutroClip, titleOutroClip2]);
+						
+						//all the tracks go in a timeline
+						let timeline = new Shotstack.Timeline;
+								timeline.setTracks([watermarkTrack, mainTrack]);
+
+						//output is required
+						let output = new Shotstack.Output;
+							output.setFormat('mp4')
+    						output.setResolution('1080');
+							
+						//The timeline and output go into an Edit
+						let edit = new Shotstack.Edit;
+							edit.setTimeline(timeline)
+    						edit.setOutput(output);
+						//send the video and the images & text to ShotStack
+						api.postRender(edit).then((data) => {
+							let message = data.response.message;
+							let id = data.response.id
+							
+							console.log(message + '\n');
+							console.log('>> Now check the progress of your render by running:');
+							console.log('>> node examples/status.js ' + id);
+
+							//checking status
+							checkStatus(id);
+							
+
+						
+						}, (error) => {
+							console.error('Request failed: ', error);
+							process.exit(1);
+						});
+
+						function checkStatus(id){
+
+							api.getRender(id).then((data) => {
+								let status = data.response.status;
+								let url = data.response.url;
+							
+								console.log('Status: ' + status.toUpperCase() + '\n');
+							
+								if (status == 'done') {
+									console.log('>> Asset URL: ' + url);
+									updateApiVideo(url);
+									
+								} else if (status == 'failed') {
+									console.log('>> Something went wrong, rendering has terminated and will not continue.');
+								} else {
+									console.log('>> Rendering in progress, please try again shortly.\n>> Note: Rendering may take up to 1 minute to complete.');
+									setTimeout(checkStatus,2000,id);
+								}
+							}, (error) => {
+								console.error('Request failed or not found: ', error);
+								process.exit(1);
+							});
+						}
+						function updateApiVideo(url){
+							//ok so the video is created at Stostack
+							const videoCreationPayload = {
+								title: title, // The title of your new video.
+								description: descr, // A brief description of your video.
+								source: url, // If you add a video already on the web, this is where you enter the url for the video.
+								
+							}; 
+							finalUpload =  client.videos.create(videoCreationPayload);
+							finalUpload.then (function(finalVideo) {
+								console.log(finalVideo);
+								var finalvideoId = finalVideo.videoId;
+								var finalPlayerURL = finalVideo.assets.finalPlayerURL;
+								console.log(finalvideoId);
+
+								res.end(finalPlayerURL);
+
+
+							});
+						}
+
+					});
+
+
+				 });
+				
+
+				 	
+			}
+		}
+		if(!foundMatch){
+			//no match yet, so wait 2 seconds and try again
+			//not encoded yet, wait 2 sec and re-reun checkMp4
+			console.log("no webhook yet.");
+			setTimeout(checkWebhook,2000,videoId, videoQuality, webhooks);
+		}
+	}
+	checkWebhook(videoId, encodingType, webhooks);
+});
+
+
+//apivideo webhook
+var webhooks = [];
+webhookResponse = {"event":"intro", 
+			"emittedAt": Date.now, 
+			"videoId":"12345",
+			"encoding":"hls",
+			"quality": "200"
+			}
+webhooks.push(webhookResponse);
+//receive a webhook that encoding is ready
+app.post("/receive_webhook", function (request, response) {
+	console.log("new video event from api.video");
+
+	let body =request.body;
+	console.log((body));
+	let type = body.type;
+	let emittedAt = body.emittedAt;
+	let webhookResponse="";
+	let liveStreamId = "";
+	let liveStreamStatus = false;
+	//we're only getting video.encoding.quality.completed right now.. but let's be careful in case the webhook changes
+
+	if (type =="video.encoding.quality.completed"){
+	  let videoId = body.videoId;
+	  let encoding = body.encoding;
+	  let quality = body.quality;
+	  liveStreamId = body.liveStreamId;
+	  webhookResponse = {"event":type, 
+	  					"emittedAt": emittedAt, 
+						"videoId":videoId,
+						"encoding":encoding,
+						"quality": quality
+						}
+	  
+	} 
+	webhooks.push(webhookResponse);
+	response.sendStatus(200);  
+  });
 
 
 //testing on 3021
