d09821b3c450f104f10a16eee8b636d0f3f78167
dougsillars <doug.sillars@gmail.com>
2021-09-23 14:14:11 -0400

    ready to try webhook

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ const rateLimit = require("express-rate-limit");
 app.use(
 	rateLimit({
 	  windowMs:  60 * 60 * 1000, // 1 hour duration in milliseconds
-	  max: 5,
-	  message: "You exceeded 5 requests in 1 hour.",
+	  max: 105,
+	  message: "You exceeded 105 requests in 1 hour.",
 	  headers: true,
 	})
   );
