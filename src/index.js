c083ad4e51ec263aba591d04c5a933d03b15760d
dougsillars <doug.sillars@gmail.com>
2021-09-23 15:58:59 -0400

    ready to try webhook

--- src/index.js
+++ src/index.js
@@ -444,7 +444,7 @@ app.post('/videoprogress', (req,res) => {
 									
 	//check the webhook to see if 720p is ready
 	for(var i=0;i<webhooks.length;i++){
-		if(webhooks[i].videoId === idToCheck && webhooks[i].quality === "720p")
+		if(webhooks[i].idToCheck === idToCheck && webhooks[i].quality === "720p")
 		{
 			console.log("final video ready");
 			currentStatusJson.readyToPlay = true;
