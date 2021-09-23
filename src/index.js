5491eea7b3bdc1c0cd3515301d6b167724d04b9a
dougsillars <doug.sillars@gmail.com>
2021-09-23 15:21:28 -0400

    ready to try webhook

--- src/index.js
+++ src/index.js
@@ -168,15 +168,6 @@ function videoStatus (videocreationList, videoId){
 		if(videocreationList[i].initialvideoid ==videoId){
 			//matched the video
 			console.log("matched a video status");
-			//check the webhok to see if final video is ready
-			//check the webhook to see if 720p is ready
-			var finalVideoId = videocreationList[i].finalVideoId
-			for(var i=0;i<webhooks.length;i++){
-				if(webhooks[i].videoId === finalVideoId && webhooks[i].quality === "720p"){
-					videocreationList[i].readyToPlay = true;
-				}
-			}
-
 			console.log(JSON.stringify(videocreationList[i]));
 			return(JSON.stringify(videocreationList[i]));
 
@@ -444,7 +435,13 @@ app.post('/videoprogress', (req,res) => {
 	//get the original videoId
 	var idToCheck = req.body.idToCheck;
 	console.log ("idToCheck",idToCheck);
-
+	//check the webhook to see if 720p is ready
+	for(var i=0;i<webhooks.length;i++){
+		if(webhooks[i].videoId === idToCheck && webhooks[i].encoding === "720p")
+		{
+			webhooks[i].readyToPlay = true;
+		}
+	}
 	currentStatus = videoStatus(videocreationList, idToCheck);
 	console.log("currentStatus",currentStatus)
 
